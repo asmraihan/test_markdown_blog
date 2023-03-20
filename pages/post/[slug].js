c9a1555eb0b9ea57dd8099932538b3c8f0d06ed1
asmraihan <asmraihanctg14@gmail.com>
2023-03-20 18:15:56 +0600

    8

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -30,7 +30,7 @@ export async function getStaticProps({ params: { slug }}) {
 
 export default function PostPage({ frontmatter, content }) {
     return (
-        <div className='prose mx-auto'>
+        <div className='w-3/4 mx-auto'>
             <h1>{frontmatter.title}</h1>
             <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
         </div>
