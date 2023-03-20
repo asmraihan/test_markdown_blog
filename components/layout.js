d9ec8b4093fe800b781577499324ca6815b62a61
asmraihan <asmraihanctg14@gmail.com>
2023-03-20 16:05:38 +0600

    first commit

---
+++ components/layout.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default function Layout({ children }) {
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <header className='bg-gray-900 mb-8 py-4'>
+        <div className='container mx-auto flex justify-center text-white'>
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+          <span className='mx-auto'>Testing Blog</span>{' '}
+        </div>
+      </header>
+      <main className='container mx-auto flex-1'>{children}</main>
+      <footer className='bg-gray-900 mt-8 py-4'>
+        <div className='container mx-auto flex justify-center text-white'>
+          &copy; 2022 Asm
+        </div>
+      </footer>
+    </div>
+  );
+}
